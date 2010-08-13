05e8eef9de16f861838d8f320aa522e658856a3f
Evan Carmi <evan@ecarmi.org>
2010-08-13 00:47:16 -0700

    Added urls mapping to 404 and 500 errors to allow testing.

--- urls.py
+++ urls.py
@@ -16,26 +16,32 @@ urlpatterns = patterns('',
 if 'django.contrib.admin' in settings.INSTALLED_APPS:
     from django.contrib import admin
     admin.autodiscover()
-    
+
     if 'django.contrib.admindocs' in settings.INSTALLED_APPS:
         urlpatterns += patterns('',
             url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
         )
-    
+
     urlpatterns += patterns('',
         url(r'^admin/', include(admin.site.urls)),
     )
 
 
-## Static Media
-# Serve up static media directly from the development server if running in
-# DEBUG mode. If you're running this on a staging site behind a proper web
-# server, there's no danger in this, since the static media URLs will be picked
-# up by that first.
+## Static Media and Error Pages
 
 if settings.DEBUG:
     import re
-    
+
+    # Allows testing of 404 and 500 error pages.
+    urlpatterns += patterns('',
+        (r'^500/$', 'django.views.defaults.server_error'),
+        (r'^404/$', 'django.views.generic.simple.direct_to_template', {'template': '404.html'}),
+    )
+
+    # Serve up static media directly from the development server if running in
+    # DEBUG mode. If you're running this on a staging site behind a proper web
+    # server, there's no danger in this, since the static media URLs will be picked
+    # up by that first.
     urlpatterns += patterns('django.views.static',
         url(r'^%s(?P<path>.*)$' % re.escape(settings.MEDIA_URL.lstrip('/')),
             'serve', {'document_root': settings.MEDIA_ROOT}),
